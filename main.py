82399c945e7760fe5f2edc2f6fd10f355a04a938
Andrzej Pustelnik <andrzej.pustelnik@student.uj.edu.pl>
2024-10-10 16:46:21 +0200

    Adding dt and clock

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         pygame.Surface.fill(screen, (255, 0, 255))
         pygame.display.flip()
@@ -19,6 +23,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
